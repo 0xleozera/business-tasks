3cf36309242c47cdfc4d192836fcd165edd5088d
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-12-01 20:06:15 -0200

    add (*) all validations

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const BaseExceptionHandler = use('BaseExceptionHandler');
-const Youch = use('Youch');
+//const Youch = use('Youch');
 const Env = use('Env');
 
 class ExceptionHandler extends BaseExceptionHandler {
@@ -10,12 +10,12 @@ class ExceptionHandler extends BaseExceptionHandler {
       return response.status(error.status).send(error.messages);
     }
 
-    if (Env.get('NODE_ENV') === 'development') {
-      const youch = new Youch(error, request.request);
-      const errorJSON = await youch.toJSON();
+    // if (Env.get('NODE_ENV') === 'development') {
+    //   const youch = new Youch(error, request.request);
+    //   const errorJSON = await youch.toJSON();
 
-      return response.status(error.status).send(errorJSON);
-    }
+    //   return response.status(error.status).send(errorJSON);
+    // }
 
     return response.status(error.status);
   }
--- start/routes.js
+++ start/routes.js
@@ -3,18 +3,18 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User');
-Route.post('sessions', 'SessionController.store');
+Route.post('sessions', 'SessionController.store').validator('Session');
 
-Route.post('forgot-password', 'ForgotPasswordController.store');
-Route.put('passwords', 'ForgotPasswordController.update');
+Route.post('forgot-password', 'ForgotPasswordController.store').validator('ForgotPassword');
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword');
 
 Route.get('files/:id', 'FileController.show');
 
 Route.group(() => {
   Route.post('files', 'FileController.store');
 
-  Route.resource('projects', 'ProjectController').apiOnly();
-  Route.resource('projects.tasks', 'TaskController').apiOnly();
+  Route.resource('projects', 'ProjectController').apiOnly().validator(new Map([[['projects.store'], ['Project']]]));
+  Route.resource('projects.tasks', 'TaskController').apiOnly().validator(new Map([[['projects.task.store'], ['Task']]]));
 }).middleware(['auth']);
 
 
