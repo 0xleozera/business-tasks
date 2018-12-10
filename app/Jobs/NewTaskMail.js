38b132ec6b4416847f8512ededd79d4081171112
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-12-10 18:54:15 -0200

    add (task) redis configure & create job to send email

---
+++ app/Jobs/NewTaskMail.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Mail = use('Mail');
+const Helpers = use('Helpers');
+
+class NewTaskMail {
+  static get concurrency () {
+    return 1
+  }
+
+  static get key () {
+    return 'NewTaskMail-job'
+  }
+
+  async handle ({ username, title, file }) {
+    console.log(`Job: ${NewTaskMail.key}`);
+
+    await Mail.send(
+      ['emails.new_task'],
+      { username, title, hasAttachment: !!file },
+      message => {
+        message.to(email).from('leoprocha100@gmail.com', 'Leonardo Pinheiro').subject('Nova tarefa pra você')
+
+        if (file) {
+          message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+            filename: file.name
+          });
+        }
+      }
+    );
+  }
+}
+
+module.exports = NewTaskMail
+
--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,6 @@
 'use strict'
-
-const Mail = use('Mail');
-const Helpers = use('Helpers');
+const Kue = use('Kue');
+const Job = use('App/Jobs/NewTaskMail');
 
 const TaskHook = exports = module.exports = {}
 
@@ -13,17 +12,5 @@ TaskHook.sendNewTaskMail = async (taskInstance) => {
 
   const { title } = taskInstance;
 
-  await Mail.send(
-    ['emails.new_task'],
-    { username, title, hasAttachment: !!file },
-    message => {
-      message.to(email).from('leoprocha100@gmail.com', 'Leonardo Pinheiro').subject('Nova tarefa pra você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        });
-      }
-    }
-  );
+  Kue.dispatch(Job.key, { email, username, file, title }, { attempts: 3 });
 }
